4d50d63ce75456cc7412275eb47b12ba094e4122
jakedupuy <jake.dupuy@gmail.com>
2016-08-06 13:14:02 -0500

    updated gather moids for to use built in vmware module utils

--- library/vcenter_gather_moids.py
+++ library/vcenter_gather_moids.py
@@ -19,34 +19,18 @@
 
 __author__ = 'yfauser'
 
-
-from pyVim.connect import SmartConnect
-from pyVmomi import vim, vmodl
-import requests
-import ssl
-
+try:
+    from pyVmomi import vim, vmodl
+    HAS_PYVMOMI = True
+except ImportError:
+    HAS_PYVMOMI = False
 
 
 VIM_TYPES = {'datacenter': [vim.Datacenter],
              'dvs_name': [vim.dvs.VmwareDistributedVirtualSwitch],
              'datastore_name': [vim.Datastore],
-             'resourcepool_name': [vim.ResourcePool]}
-
-
-def get_cluster_mo(datacenter_mo, cluster_name):
-    host_folder = datacenter_mo.hostFolder
-    for child in host_folder.childEntity:
-        if child.name == cluster_name:
-            return child
-    return None
-
-
-def get_portgroup_mo(datacenter_mo, portgroup_name):
-    network_folder = datacenter_mo.network
-    for network in network_folder:
-        if network.name == portgroup_name:
-            return network
-    return None
+             'resourcepool_name': [vim.ResourcePool],
+             'portgroup_name': [vim.dvs.DistributedVirtualPortgroup]}
 
 
 def get_mo(content, searchedname, vim_type_list):
@@ -57,65 +41,43 @@ def get_mo(content, searchedname, vim_type_list):
     return None
 
 
-def get_all_objs(content, vimtype):
-    obj = {}
-    container = content.viewManager.CreateContainerView(content.rootFolder, vimtype, True)
-    for managed_object_ref in container.view:
-        obj.update({managed_object_ref: managed_object_ref.name})
-    return obj
-
-
-def connect_to_api(vchost, vc_user, vc_pwd):
-    requests.packages.urllib3.disable_warnings()
-    if hasattr(ssl, 'SSLContext'):
-     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-     context.verify_mode = ssl.CERT_NONE
-    else:
-        context = None
-    if context:
-        service_instance = SmartConnect(host=vchost, user=vc_user, pwd=vc_pwd, sslContext=context)
-    else:
-        service_instance = SmartConnect(host=vchost, user=vc_user, pwd=vc_pwd)
-
-
-    return service_instance.RetrieveContent()
-
-
 def main():
-    module = AnsibleModule(
-        argument_spec=dict(
-            hostname=dict(required=True),
-            username=dict(required=True),
-            password=dict(required=True, no_log=True),
+
+    argument_spec = vmware_argument_spec()
+
+    argument_spec.update(
+        dict(
             datacenter_name=dict(required=True),
             cluster_name=dict(type='str'),
             resourcepool_name=dict(type='str'),
             dvs_name=dict(type='str'),
             portgroup_name=dict(type='str'),
             datastore_name=dict(type='str')
-        ),
+        )
+    )
+
+    module = AnsibleModule(
+        argument_spec=argument_spec,
         required_one_of=[['cluster_name', 'portgroup_name', 'resourcepool_name', 'dvs_name', 'datastore_name']],
         mutually_exclusive=[['cluster_name', 'portgroup_name', 'resourcepool_name', 'dvs_name', 'datastore_name']],
         supports_check_mode=False
     )
 
-    try:
-        content = connect_to_api(module.params['hostname'], module.params['username'], module.params['password'])
-    except vim.fault.InvalidLogin:
-        module.fail_json(msg='exception while connecting to vCenter, login failure, check username and password')
-    except requests.exceptions.ConnectionError:
-        module.fail_json(msg='exception while connecting to vCenter, check hostname, FQDN or IP')
+    if not HAS_PYVMOMI:
+        module.fail_json(msg='pyvmomi is required for this module')
+
+    content = connect_to_api(module)
 
     searched_parameters = ['cluster_name', 'portgroup_name', 'resourcepool_name', 'dvs_name', 'datastore_name']
 
-    datacenter_mo = get_mo(content, module.params['datacenter_name'], VIM_TYPES['datacenter'])
+    datacenter_mo = find_datacenter_by_name(content, module.params['datacenter_name'])
     datacenter_moid =  datacenter_mo._moId
 
     for searched_parameter in searched_parameters:
         if searched_parameter == 'cluster_name' and module.params[searched_parameter]:
-            object_mo = get_cluster_mo(datacenter_mo, module.params[searched_parameter])
+            object_mo = find_cluster_by_name_datacenter(datacenter_mo, module.params[searched_parameter])
         elif searched_parameter == 'portgroup_name' and module.params[searched_parameter]:
-            object_mo = get_portgroup_mo(datacenter_mo, module.params[searched_parameter])
+            object_mo = get_mo(content, module.params[searched_parameter], VIM_TYPES[searched_parameter])
         elif searched_parameter and module.params[searched_parameter]:
             object_mo = get_mo(content, module.params[searched_parameter], VIM_TYPES[searched_parameter])
 
@@ -124,6 +86,7 @@ def main():
     module.exit_json(changed=False, object_id=object_id, datacenter_moid=datacenter_moid)
 
 from ansible.module_utils.basic import *
+from ansible.module_utils.vmware import *
 
 if __name__ == '__main__':
     main()
