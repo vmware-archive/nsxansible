14ec54d3a8dec57d9304928e24b5029fd9fe4835
Chris Mutchler <chris@virtualelephant.com>
2018-01-10 21:53:57 -0800

    Initial commit, not yet functional.

--- library/nsx_edge_nat.py
+++ library/nsx_edge_nat.py
@@ -22,7 +22,59 @@ def get_edge(client_session, edge_name):
     return edge_id, edge_params
 
 
-def create_nat_rule(client_session, esg_id, rule_type, vnic, protocol, source_ip, source_port, dest_port, ip_addr, port_range):
+def create_nat_rule(client_session, edge_name, nat_enabled, loggingEnabled, action, vnic, originalAddress, translatedAddress,
+                    dnatMatchSourceAddress, snatMatchAddress, protocol, icmpType, originalPort,
+                    translatedPort, dnatMatchSourcePort, snatMatchDestinationPort):
+    """
+    :param enabled: Enable rule. Boolean. Default is true.
+    :param loggingEnabled: Enable logging. Default is false.
+    :param action: Type of NAT. Values can be 'snat' or 'dnat'
+    :param vnic: Interface on which the translation is applied.
+    :param originalAddress: Original address or range. Default is 'any'
+    :param translatedAddress: Translated address or range. Default is 'any'.
+    :param dnatMatchSourceAddress: Source address to match in DNAT rules. Default is 'any'.
+    :param snatMatchDestinationAddress: Destination address to match in SNAT address. Default is 'any'.
+    :param protocol: Protocol. Default is 'any'.
+    :param icmpType: ICMP type. Only supported when protocol == 'icmp'. Default is 'any'.
+    ;param originalPort: Source port for SNAT, destination port for DNAT. Default is 'any'.
+    :param translatedPort: Translated port. Default is 'any'.
+    :param dnatMatchSourcePort: Source port in DNAT rules. Not valid in SNAT rules. Default is 'any'.
+    :param snatMatchDestinationPort: Destination port in SNAT rules. Not valid in DNAT fules. Default is 'any'.
+    """
+    edge_id, edge_params = get_edge(client_session, edge_name)
+
+    if action == 'snat':
+        nat_rule_dict = {'enabled': nat_enabled,
+                         'loggingEnabled': loggingEnabled,
+                         'action': action,
+                         'vnic': vnic,
+                         'originalAddress': originalAddress,
+                         'translatedAddress': translatedAddress,
+                         'snatMatchDestinationAddress': snatMatchDestinationAddress,
+                         'protocol': protocol,
+                         'originalPort': originalPort,
+                         'translatedPort': translatedPort,
+                         'snatMatchDestinationPort': snatMatchDestinationPort
+                        }
+        if protocol == 'icmp':
+            nat_rule_dict['icmpType'] = icmpType
+    elif action == 'dnat':
+        nat_rule_dict = {'enabled': nat_enabled,
+                         'loggingEnabled': loggingEnabled,
+                         'action': action,
+                         'vnic': vnic,
+                         'originalAddress': originalAddress,
+                         'translatedAddress': translatedAddress,
+                         'dnatMatchSourceAddress': dnatMatchSourceAddress,
+                         'protocol': protocol,
+                         'originalPort': originalPort,
+                         'translatedPort': translatedPort,
+                         'dnatMatchSourcePort': dnatMatchSourcePort
+                        }
+        if protocol == 'icmp':
+            nat_rule_dict['icmpType'] = icmpType
+
+    cfg_result = client_session.create('nat', uri_parameters={'edgeId': edge_id}, request_body_dict={'natRule': nat_rule_dict})
 
     if cfg_result['status'] == 204:
         return True
@@ -34,15 +86,21 @@ def main():
             argument_spec=dict(
                 state=dict(default='present', choices=['present', 'absent']),
                 nsxmanager_spec=dict(required=True, no_log=True, type='dict'),
-                name=dict(required=True),
+                edge_name=dict(required=True),
+                nat_enabled=dict(default=True),
+                loggingEnabled=dict(default=False),
+                action=dict(required=True, choices=['dnat', 'snat']),
                 vnic=dict(),
-                protocol=dict(default='any'),
-                source_ip=dict(required=True),
-                source_port=dict(default='any'),
-                dest_port=dict(default='any'),
-                ip_addr=dict(required=True),
-                port_range=dict(),
-                rule_type=dict(required=True, choices=['dnat', 'snat'])
+                originalAddress=dict(default='any'),
+                translatedAddress=dict(default='any'),
+                dnatMatchSourceAddress=dict(default='any'),
+                snatMatchDestinationAddress=dict(default='any'),
+                procotol=dict(default='any'),
+                icmpType=dict(default='any'),
+                originalPort=dict(default='any'),
+                translatedPort=dict(default='any'),
+                dnatMatchSourcePort=dict(default='any'),
+                snatMatchDestinationPort=dict(default='any')
             ),
             supports_check_mode=False
     )
